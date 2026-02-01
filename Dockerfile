43d66c90a6d60d92868c4a3eae1e0fd7ef55b8af
Hanne Soubry <hanne.soubry@student.vives.be>
2026-02-01 14:43:16 +0100

    Add API and docker file

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+#Use a base image with Python and FastAPI installed
+FROM tiangolo/uvicorn-gunicorn-fastapi:python3.8
+
+# Copy the application files and the model into the container
+COPY ./main.py /app/main.py
+COPY ./model.pkl /app/model.pkl
+
+# install other packages
+RUN pip install numpy
+RUN pip install tensorflow
+
+# The command to run the application
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import numpy as np
+
+# Load the pretrained model
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+# Create the app
+app = FastAPI()
+
+# Define endpoint
+@app.post("/predict")
+def predict(data: List[float]):
+    data = np.array(input.data).reshape(1, -1)
+    prediction = model.predict(data)
+    return {"prediction": prediction}
